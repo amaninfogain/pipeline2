327b572fff071790cb722b18549517f1750bc203
amaninfogain <33146795+amaninfogain@users.noreply.github.com>
2019-01-24 14:22:04 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,22 +1,15 @@
 package org.kodejava.example.commons.io;
-
 import org.apache.commons.io.FileUtils;
-
 import java.io.File;
 import java.io.IOException;
 
-public class test {
-    public static void main(String[] args) {
- 
+def func ()
+       {
         String source = "C:/Users/abisht2/Desktop/source";
         File srcDir = new File(source);  
         String destination = "C:/Users/abisht2/Desktop/destination";
         File destDir = new File(destination);
-
-        try {
-          FileUtils.copyDirectory(srcDir, destDir);
-        } catch (IOException e) {
-            e.printStackTrace();
+        FileUtils.copyDirectory(srcDir, destDir);   
         }
-    }
-}
+
+
