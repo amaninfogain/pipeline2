8fb1440ecd4bcf1ec23d14ced332efd15bbc1164
amaninfogain <33146795+amaninfogain@users.noreply.github.com>
2019-01-24 14:23:33 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -3,7 +3,7 @@ import org.apache.commons.io.FileUtils;
 import java.io.File;
 import java.io.IOException;
 
-def func ()
+def func()
        {
         String source = "C:/Users/abisht2/Desktop/source";
         File srcDir = new File(source);  
