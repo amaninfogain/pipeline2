e08dfb4fd267526f6644745e8697c2a88876b4ea
amaninfogain <33146795+amaninfogain@users.noreply.github.com>
2019-01-24 12:02:39 +0530

    Update copy.groovy

--- vars/copy.groovy
+++ vars/copy.groovy
@@ -4,5 +4,5 @@ import java.nio.file.*
 def call(String name, String name2){
  echo "Hello ${name}"
   echo "Hello ${name2}"
- Files.copy(${name}, ${name2}, StandardCopyOption.COPY_ATTRIBUTES)
+ Files.copy(${name}, ${name2})
 }
