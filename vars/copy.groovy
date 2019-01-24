1fbdb8da22a6463ce92ebd1723b6af7404d3fc1f
amaninfogain <33146795+amaninfogain@users.noreply.github.com>
2019-01-24 12:07:11 +0530

    Update copy.groovy

--- vars/copy.groovy
+++ vars/copy.groovy
@@ -1,8 +1,10 @@
-import hudson.model.*
+import java.nio.file.Paths
+import java.nio.file.Files
 import java.nio.file.*
 
 def call(String name, String name2){
  echo "Hello ${name}"
   echo "Hello ${name2}"
- Files.copy(${name}, ${name2})
+
+ Files.copy(Paths.get(${name}), Paths.get(${name2}))
 }
