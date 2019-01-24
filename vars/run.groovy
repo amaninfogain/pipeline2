35e404c914a31b5a5c354de88f644aa85bf7bb46
amaninfogain <33146795+amaninfogain@users.noreply.github.com>
2019-01-24 14:45:53 +0530

    Update run.groovy

--- vars/run.groovy
+++ vars/run.groovy
@@ -8,7 +8,7 @@ def call(String name, String name2){
  echo "Hello ${name}"
   echo "Hello ${name2}"
 
-Files.copy("${name}", "${name2}")
+//Files.copy("${name}", "${name2}")
  //FileUtils.copyDirectory("/home/xavient/git/pipeline/data", "/home/xavient/git/pipeline/data2");
 }
 
