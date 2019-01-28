1245f8894f0cc48638a8cc0410bf60300b9b29d2
amaninfogain <33146795+amaninfogain@users.noreply.github.com>
2019-01-28 11:27:28 +0530

    Update run.groovy

--- vars/run.groovy
+++ vars/run.groovy
@@ -7,7 +7,7 @@ import java.io.*
 def call(String name, String name2){
  echo "Hello ${name}"
   echo "Hello ${name2}"
-println "cp -rf /home/xavient/git/pipeline/data /home/xavient/git/pipeline/data2.execute().waitfor(long, java.util.concurrent.TimeUnit)"
+println "cp -rf /home/xavient/git/pipeline/data4 /home/xavient/git/pipeline/data2.execute().waitfor(long, java.util.concurrent.TimeUnit)"
 //Files.copy("${name}", "${name2}")
  //FileUtils.copyDirectory("/home/xavient/git/pipeline/data", "/home/xavient/git/pipeline/data2");
 }
